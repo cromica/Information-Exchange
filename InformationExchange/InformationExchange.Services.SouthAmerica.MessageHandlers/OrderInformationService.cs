eb5edb2a0291fd94aa20894e4060b78597b166ab
cromica <cromica@gmail.com>
2014-05-02 20:16:59 +0300

    implement south america legacy system facade

---
+++ InformationExchange/InformationExchange.Services.SouthAmerica.MessageHandlers/OrderInformationService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InformationExchange.Services.SouthAmerica.MessageHandlers.DataAccess;
+using com.iex.orders.getorders;
+using com.iex.orders.order;
+
+namespace InformationExchange.Services.SouthAmerica.MessageHandlers
+{
+    public class OrderInformationService
+    {
+        public OrderResponse GetOrders(OrderRequest request)
+        {
+            var response = new OrderResponse { Orders = new ArrayOfOrders() };
+
+            var context = new OrderManagementSouthAmericaEntities();
+            var orders = context.Orders.ToList();
+            orders.ForEach(order =>
+            {
+                var responseOrder = new com.iex.orders.order.Order
+                {
+                    Id = order.Id.ToString(),
+                    Name = order.Name,
+                    Items = order.Items,
+                    Value = order.Value,
+                    Country =order.Country_Code,
+                    Region = "South America"
+                };
+
+                response.Orders.Add(responseOrder);
+            });
+
+            return response;
+        }
+    }
+}
--- InformationExchange/InformationExchange.Services.SouthAmerica.MessageHandlers/Properties/AssemblyInfo.cs
+++ InformationExchange/InformationExchange.Services.SouthAmerica.MessageHandlers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InformationExchange.Test")]
+[assembly: AssemblyTitle("InformationExchange.Services.SouthAmerica.MessageHandlers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InformationExchange.Test")]
+[assembly: AssemblyProduct("InformationExchange.Services.SouthAmerica.MessageHandlers")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("06f20f5f-090b-416a-92d2-4de473482554")]
+[assembly: Guid("4446a45b-d3e2-4888-b5e4-013c1e927ba3")]
 
 // Version information for an assembly consists of the following four values:
 //
